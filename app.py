6621de157954e5944fe95e7aa1fc26e18d675dd0
Claire Burghard <claireburghard@gmail.com>
2014-10-18 15:06:34 -0400

    worked on database stuff for posts, there are errors

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request
+import sqlite3
+import csv
 
 app = Flask(__name__)
+
+conn = sqlite3.connect('database.db')
+c=conn.cursor();
+
+def create():
+    q = "create table posts (name TEXT, title TEXT, blogpost TEXT)"
+    c.execute(q)
+    conn.commit()
+
+def add(n,t,b):
+    q = "INSERT INTO posts VALUES("
+    q += n + ","
+    q += t + ","
+    q += b + ")"
+    c.execute(q)
+    conn.commit()
 
 #index page
 @app.route("/", methods=["GET","POST"])
@@ -10,15 +28,18 @@ def index():
     else:
         button = request.form["b"]
         name = request.form["name"]
+        title = request.form["title"]
         blogpost = request.form["blogpost"]
         if button=="cancel":
             return render_template("index.html")
         else:
-            return render_template("posttest.html",
-                                   name = name,
-                                   text = blogpost)
-
+            add(name,title,blogpost)
+            link = "<a href='http://localhost:5000/'" + title + ">here</a>"
+            return render_template("postadded.html",link=link)
 
 if __name__=="__main__":
     app.debug = True
     app.run()
+
+conn.commit()
+
