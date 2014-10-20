b437056615830d9354d1cf2f0561e08b1b2ee0ca
Claire Burghard <claireburghard@gmail.com>
2014-10-20 19:00:32 -0400

    working on user accounts

--- app.py
+++ app.py
@@ -4,48 +4,47 @@ import csv
 
 app = Flask(__name__)
 
-#set up (this should go into a different file later
 conn = sqlite3.connect("database.db")
 c = conn.cursor()
-#q = "create table User(username TEXT, password TEXT)"
+q = "create table if not exists User(username TEXT, password TEXT)"
 t = "create table if not exists posts(name TEXT, title TEXT, blogpost TEXT)"
-#c.execute(q)
+c.execute(q)
 c.execute(t)
 
 #login page
-#@app.route("/", methods=["GET","POST"])
-#@app.route("/login", methods=["GET","POST"])
-#def login():
- #   if request.method=="GET":
-  #      return render_template("login.html", message = "")
-   # else:
-    #    username = request.form["username"]
-    #    password = request.form["password"]
-    #    button = request.form["b"]
-    #    if button == "Login":
-    #        validity = authenticate(username, password)
-    #        if validity == "Valid":
-    #            index(username)
-    #        else:
-    #            return render_template("login.html", message = "Username/ Password Invalid")
-    #    if button == "Register":
-    #        return redirect(url_for('register'))
+@app.route("/", methods=["GET","POST"])
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method=="GET":
+        return render_template("login.html", message = "")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        button = request.form["b"]
+        if button == "Login":
+            validity = authenticate(username, password)
+            if validity == "Valid":
+                index(username)
+            else:
+                return render_template("login.html", message = "Username/ Password Invalid")
+                if button == "Register":
+                    return redirect(url_for('register'))
 
 
-#@app.route("/register", methods=["GET", "POST"])
-#def register():
-#    if request.method=="GET":
-#        return render_template("register.html", message = "")
-#    else:
-#        username = request.form["username"]
-#        password = request.form["password"]
-#        confirm = request.form["confirm_password"]
-#        name = request.form["name"]
-#        if (confirm == password):
-#            add_user(username, password)
-#            return redirect(url_for('login'))
-#        else:
-#            return render_template("register.html", message = "Password doesn't match confirmation")
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method=="GET":
+        return render_template("register.html", message = "")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        confirm = request.form["confirm_password"]
+        name = request.form["name"]
+        if (confirm == password):
+            add_user(username, password)
+            return redirect(url_for('login'))
+        else:
+            return render_template("register.html", message = "Password doesn't match confirmation")
 
 #index page
 @app.route("/", methods=["GET","POST"])
@@ -67,29 +66,26 @@ def index():
             q += "'" + title + "',"
             q += "'" + blogpost + "')"
             c.execute(q)
-            link = "<a href='http://localhost:5000/'" + title + ">here</a>"
+            #link = "<a href='http://localhost:5000/'" + title + ">here</a>"
             return render_template("postadded.html",
-                                   name-name,
-                                   link=link)
+                                   name=name)
 
+def add_user(username, password):
+    conn = sqlite3.connect('test.db')
+    c=conn.cursor();
+    BASE="INSERT INTO User VALUES('" + username +"', '" + password + "')"
+    c.execute(BASE)
+    conn.commit()
+    return BASE + "user added"
 
-#def add_user(username, password):
-#    conn = sqlite3.connect('test.db')
-#    c=conn.cursor();
-#    BASE="INSERT INTO User VALUES('" + username +"', '" + password + "')"
-#    
-#    c.execute(BASE)
-#    conn.commit()
-#    return BASE + "user added"
-
-#def authenticate(username, password):
-#    conn = sqlite3.connect("test.db")
-#    c = conn.cursor()
-#    c.execute("SELECT * FROM User WHERE username = '%s'" % username + "and password = '%s'" % password)
-#    if c.fetchone() is None:
-#        return "Invalid"
-#    else:
-#        return "Valid"
+def authenticate(username, password):
+    conn = sqlite3.connect("test.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM User WHERE username = '%s'" % username + "and password = '%s'" % password)
+    if c.fetchone() is None:
+        return "Invalid"
+    else:
+        return "Valid"
 
     
 
